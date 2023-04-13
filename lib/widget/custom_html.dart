5b62dc34058493f550e0a5ad6b9afd0f65b31d75
Tran Tan Duc <44394323+leovu@users.noreply.github.com>
2023-04-13 10:29:42 +0700

    push
    push

--- lib/widget/custom_html.dart
+++ lib/widget/custom_html.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_html/flutter_html.dart';
+import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class CustomHtml extends StatelessWidget {
@@ -13,11 +13,9 @@ class CustomHtml extends StatelessWidget {
   Widget build(BuildContext context) {
     return html == null?Container():SingleChildScrollView(
         physics: physics??AlwaysScrollableScrollPhysics(),
-        child: Html(
-          data: html,
-          onLinkTap: (url, _, attributes, element){
-            launch(url);
-          },
+        child: HtmlWidget(
+          html,
+          onTapUrl: (url) => launch(url),
         )
     );
   }
